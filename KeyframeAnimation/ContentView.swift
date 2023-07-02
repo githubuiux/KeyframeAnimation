5d73f63c490a6ec864f64ef6f20dd3fe9a8faa53
githubuiux <uiuxista@gmail.com>
2023-07-02 17:22:52 +0300

    Initial Commit

---
+++ KeyframeAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyframeAnimation
+//
+//  Created by Levent on 2.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyframeAnimation/KeyframeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyframeAnimationApp.swift
+//  KeyframeAnimation
+//
+//  Created by Levent on 2.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct KeyframeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
